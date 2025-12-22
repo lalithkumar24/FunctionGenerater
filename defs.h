f29fb5665352d79e3832b311d98ca16ddd31a28f
lalithkumar24 <lali22048.it@rmkec.ac.in>
2025-12-23 02:49:17 +0530

    Initial commit

---
+++ defs.h
@@ -0,0 +1,13 @@
+//
+// Created by darkrai on 12/20/25.
+//
+
+#ifndef FUNCTION_GENERATER_DEFS_H
+#define FUNCTION_GENERATER_DEFS_H
+#define SCREEN_WIDTH   1280
+#define SCREEN_HEIGHT  720
+constexpr int centerX = 640;
+constexpr int centerY = 360;
+constexpr double scale = 50.0;
+constexpr int tickSize = 10;
+#endif //FUNCTION_GENERATER_DEFS_H
